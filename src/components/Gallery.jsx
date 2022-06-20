d1c7e17723fd5c0444d6a8c6953e7ebf1d282dce
HariKiran <harikiran.kante@gmail.com>
2022-06-20 18:57:47 +0530

    added height property for gallery images

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,85 +1,91 @@
-import { useEffect  } from 'react';
-import '../styles/gallery.css'
+import { useEffect } from "react";
+import "../styles/gallery.css";
 // import plus_sign from '../images/plus.jpg'
 
 // Core modules imports are same as usual
-import { Navigation } from 'swiper';
+import { Navigation } from "swiper";
 // Direct React component imports
-import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react.js";
 // Import Swiper styles
-import 'swiper/swiper-bundle.min.css';
-import 'swiper/swiper.min.css';
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
 import "swiper/modules/navigation/navigation.min.css";
 
 const Gallery = () => {
-    const lightBox = ()=>{
-        var lb = document.querySelector('.light-box');
-        var imgs = document.querySelectorAll('.gallery-img');
-        imgs.forEach((img)=>{
-        img.addEventListener("click",()=>{
-                console.log("skdj");
-                lb.classList.add('active-light-box');
-                const image = document.createElement('img');
-                image.src = img.src;
-                while(lb.firstChild){
-                    lb.removeChild(lb.firstChild);
-                }
-                lb.appendChild(image);
-            })
-        })
+  const lightBox = () => {
+    var lb = document.querySelector(".light-box");
+    var imgs = document.querySelectorAll(".gallery-img");
+    imgs.forEach((img) => {
+      img.addEventListener("click", () => {
+        console.log("skdj");
+        lb.classList.add("active-light-box");
+        const image = document.createElement("img");
+        image.src = img.src;
+        while (lb.firstChild) {
+          lb.removeChild(lb.firstChild);
+        }
+        lb.appendChild(image);
+      });
+    });
 
-        lb.addEventListener('click',(e)=>{
-        if(e.target !== e.currentTarget) return;
-        lb.classList.remove('active-light-box');
-        })
-    }
-    useEffect(()=>{
-        lightBox();
-    },[])
-    const images = [
-        'https://www.bytecodelearners.tech/statics/event2022_pic1.jpeg',
-        'https://www.bytecodelearners.tech/statics/event2022_pic2.jpeg',
-        'https://www.bytecodelearners.tech/statics/fresh2.jpeg',
-        'https://www.bytecodelearners.tech/statics/event2022_pic4.jpeg',
-        'https://www.bytecodelearners.tech/statics/event2022_pic3.jpeg',
-        'https://www.bytecodelearners.tech/statics/event2022_pic6.jpeg',
-        'https://www.bytecodelearners.tech/statics/fresh1.jpeg',
-        'https://www.bytecodelearners.tech/statics/fresh3.jpeg',
-        'https://www.bytecodelearners.tech/statics/event2022_pic5.jpeg',
-        
-    ];
-    const fresherImgs = [
-        'https://www.bytecodelearners.tech/statics/fresh4.jpeg',
-        'https://www.bytecodelearners.tech/statics/fresh5.jpeg',
-        'https://www.bytecodelearners.tech/statics/g3.jpg',
-        'https://www.bytecodelearners.tech/statics/fresh6.jpeg',
-        'https://www.bytecodelearners.tech/statics/g1.jpg',
-        'https://www.bytecodelearners.tech/statics/g5.jpg',
-        'https://www.bytecodelearners.tech/statics/g6.jpg',
-        'https://www.bytecodelearners.tech/statics/g2.jpg',
-        'https://www.bytecodelearners.tech/statics/g4.jpg',
-        'https://www.bytecodelearners.tech/statics/prevEvent1.jpg',
-        'https://www.bytecodelearners.tech/statics/prevEvent2.jpg',
-        
-    ];
-    return ( 
-        <div className="gallery" id="Gallery">
-            <h1>Event Gallery</h1>
-            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                <SwiperSlide>
-                <div  className="images">
-                {images.map((img,index)=>{return <img className='gallery-img' key={index} src={img} alt=""/>})}
-                </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                <div   className="images">
-                    {fresherImgs.map((img,index)=>{return <img className='gallery-img' key={index} src={img} alt=""/>})}
-                </div>
-                </SwiperSlide>
-            </Swiper>
-            <div className="light-box">.</div>
-        </div>
-     );
-}
- 
+    lb.addEventListener("click", (e) => {
+      if (e.target !== e.currentTarget) return;
+      lb.classList.remove("active-light-box");
+    });
+  };
+  useEffect(() => {
+    lightBox();
+  }, []);
+  const images = [
+    "https://www.bytecodelearners.tech/statics/event2022_pic1.jpeg",
+    "https://www.bytecodelearners.tech/statics/event2022_pic2.jpeg",
+    "https://www.bytecodelearners.tech/statics/fresh2.jpeg",
+    "https://www.bytecodelearners.tech/statics/event2022_pic4.jpeg",
+    "https://www.bytecodelearners.tech/statics/event2022_pic3.jpeg",
+    "https://www.bytecodelearners.tech/statics/event2022_pic6.jpeg",
+    "https://www.bytecodelearners.tech/statics/fresh1.jpeg",
+    "https://www.bytecodelearners.tech/statics/fresh3.jpeg",
+    "https://www.bytecodelearners.tech/statics/event2022_pic5.jpeg",
+  ];
+  const fresherImgs = [
+    "https://www.bytecodelearners.tech/statics/fresh4.jpeg",
+    "https://www.bytecodelearners.tech/statics/fresh5.jpeg",
+    "https://www.bytecodelearners.tech/statics/g3.jpg",
+    "https://www.bytecodelearners.tech/statics/fresh6.jpeg",
+    "https://www.bytecodelearners.tech/statics/g1.jpg",
+    "https://www.bytecodelearners.tech/statics/g5.jpg",
+    "https://www.bytecodelearners.tech/statics/g6.jpg",
+    "https://www.bytecodelearners.tech/statics/g2.jpg",
+    "https://www.bytecodelearners.tech/statics/g4.jpg",
+    "https://www.bytecodelearners.tech/statics/prevEvent1.jpg",
+    "https://www.bytecodelearners.tech/statics/prevEvent2.jpg",
+  ];
+  return (
+    <div className="gallery" id="Gallery">
+      <h1>Event Gallery</h1>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>
+          <div className="images">
+            {images.map((img, index) => {
+              return (
+                <img className="gallery-img" key={index} src={img} alt="" />
+              );
+            })}
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="images">
+            {fresherImgs.map((img, index) => {
+              return (
+                <img className="gallery-img" key={index} src={img} alt="" />
+              );
+            })}
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      <div className="light-box">.</div>
+    </div>
+  );
+};
+
 export default Gallery;
--- src/routes/ContactUs.jsx
+++ src/routes/ContactUs.jsx
@@ -1,98 +1,105 @@
-import React, { useEffect } from 'react';
-import { sticky } from '../components/Navbar';
+import React, { useEffect } from "react";
+import { sticky } from "../components/Navbar";
 const ContactUs = () => {
-    useEffect(()=>{
-        sticky();
-        },[]);
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "https://platform.twitter.com/widgets.js";
-        document.getElementsByClassName("twitter-embed")[0].appendChild(script);
-    }, []);
+  useEffect(() => {
+    sticky();
+  }, []);
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://platform.twitter.com/widgets.js";
+    document.getElementsByClassName("twitter-embed")[0].appendChild(script);
+  }, []);
 
-    return ( 
-        <div className="contact-us">
-            <div className="heading">
-                <h3>Social Media Feeds</h3>
+  return (
+    <div className="contact-us">
+      <div className="heading">
+        <h3>Social Media Feeds</h3>
+      </div>
+      <div className="boxes">
+        <div className="rectangle">
+          <div className="ions">
+            <a href="https://www.facebooke.com" className="facebook social">
+              {/* <FontAwesomeIcon icon={faFacebook} size="3x" /> */}
+              <i className="fab fa-facebook box-icons"></i>
+            </a>
+          </div>
+
+          <div className=" fb">
+            <h2>FACEBOOK FEEDS</h2>
+
+            <div className="event-container">
+              ==
+              <div className="container">
+                <iframe
+                  src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBytecodeLearners-103644071079006%2F&tabs=timeline&width=320&height=600&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId"
+                  scrolling="no"
+                  frameborder="0"
+                  allowfullscreen="true"
+                  allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                  title="contact-us"
+                ></iframe>
+              </div>
             </div>
-            <div className="boxes">
-                <div className="rectangle">
-                <div className="ions">
-                    <a href="https://www.facebooke.com" className="facebook social">
-                    {/* <FontAwesomeIcon icon={faFacebook} size="3x" /> */}
-                    <i className="fab fa-facebook box-icons"></i>
-                    </a>
-                </div>
-
-                <div className=" fb">
-                    <h2>FACEBOOK FEEDS</h2>
-
-                    <div className="event-container">
-                    ==
-                    <div className="container">
-                        <iframe
-                        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FBytecodeLearners-103644071079006%2F&tabs=timeline&width=320&height=600&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId"
-                        scrolling="no"
-                        frameborder="0"
-                        allowfullscreen="true"
-                        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-                        title="contact-us"
-                        ></iframe>
-                    </div>
-                    </div>
-                </div>
-                </div>
-
-                <div className="rectangle1">
-                <div className=" inst">
-                    <div className="ions1">
-                        <a href="https://www.instagram.com" className="instagram social">
-                            {/* <FontAwesomeIcon icon={faInstagram} size="3x" /> */}
-                            <i className="fab fa-instagram box-icons"></i>
-                        </a>
-                    </div>
-
-                    <h2>INSTAGRAM FEEDS</h2>
-                    <scrollbar>
-                    <div className="event-container">
-                    
-                        <div className="container">
-                        <div className="scroller">
-                            <ul>
-                                <div class="elfsight-app-41f24f40-af42-4831-abdf-d8732bba57e8"></div>
-                            </ul>
-                        </div>
-                        </div>
-                    </div>
-                    </scrollbar>
-                </div>
-                </div>
-
-                <div className="rectangle2">
-                <div className=" twit">
-                    <div className="ions2">
-                        <a
-                            href="https://twitter.com/ByteCodeLearner?ref_src=twsrc%5Etfw"
-                            className="twitter social" target="_blank" rel="noopener noreferrer">
-                            {/* <FontAwesomeIcon icon={faTwitter} size="3x" /> */}
-                            <i className="fab fa-twitter box-icons"></i>
-                        </a>
-                    </div>
-
-                    <h2>TWITTER FEEDS</h2>
-                    <div className="container">
-                    <div className="scroller">
-                        <ul>
-                        <a class="twitter-timeline twitter-embed" href="https://twitter.com/ByteCodeLearner?ref_src=twsrc%5Etfw" data-show-count="false">Tweets by ByteCodeLearner</a> 
-                
-                        </ul>
-                    </div>
-                    </div>
-                </div>
-                </div>
-            </div>
+          </div>
         </div>
-     );
-}
- 
+
+        <div className="rectangle1">
+          <div className=" inst">
+            <div className="ions1">
+              <a href="https://www.instagram.com" className="instagram social">
+                {/* <FontAwesomeIcon icon={faInstagram} size="3x" /> */}
+                <i className="fab fa-instagram box-icons"></i>
+              </a>
+            </div>
+
+            <h2>INSTAGRAM FEEDS</h2>
+            <scrollbar>
+              <div className="event-container">
+                <div className="container">
+                  <div className="scroller">
+                    <ul>
+                      <div class="elfsight-app-41f24f40-af42-4831-abdf-d8732bba57e8"></div>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </scrollbar>
+          </div>
+        </div>
+
+        <div className="rectangle2">
+          <div className=" twit">
+            <div className="ions2">
+              <a
+                href="https://twitter.com/ByteCodeLearner?ref_src=twsrc%5Etfw"
+                className="twitter social"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {/* <FontAwesomeIcon icon={faTwitter} size="3x" /> */}
+                <i className="fab fa-twitter box-icons"></i>
+              </a>
+            </div>
+
+            <h2>TWITTER FEEDS</h2>
+            <div className="container">
+              <div className="scroller">
+                <ul>
+                  <a
+                    class="twitter-timeline twitter-embed"
+                    href="https://twitter.com/ByteCodeLearner?ref_src=twsrc%5Etfw"
+                    data-show-count="false"
+                  >
+                    Tweets by ByteCodeLearner
+                  </a>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default ContactUs;
